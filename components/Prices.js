3f1b1531935cb9ac8ba46822696eed3c8baf5d5e
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-01-28 11:36:02 -0800

    isimorphic fetch implemented

--- components/Prices.js
+++ components/Prices.js
@@ -4,14 +4,34 @@ class Prices extends React.Component {
   }
 
   render() {
-
+    let list = '';
+    switch(this.state.currency){
+      case 'USD': 
+      console.log('we\'re doing a thing')
+      list = <li className="list-group-item">Bitcoin rate for {this.props.bpi.USD.description} : 
+      <span className="badge badge-primary">{this.props.bpi.USD.code}</span> 
+      <strong>{this.props.bpi.USD.rate}</strong>
+      </li>
+        break;
+      case 'GBP':
+        list = <li className="list-group-item">Bitcoin rate for {this.props.bpi.GBP.description} : 
+      <span className="badge badge-primary">{this.props.bpi.GBP.code}</span> 
+      <strong>{this.props.bpi.GBP.rate}</strong>
+      </li>
+        break;
+      case 'EUR':
+        list = <li className="list-group-item">Bitcoin rate for {this.props.bpi.EUR.description} : 
+        <span className="badge badge-primary">{this.props.bpi.EUR.code}</span> 
+        <strong>{this.props.bpi.EUR.rate}</strong>
+        </li>
+        break;
+    }
+    console.log(`props is`, this.props)
     return (
       <div>
+        Prices
         <ul className="list-group">
-          <li className="list-group-item">Bitcoin rate for {this.props.bpi[this.state.currency].description} : 
-          <span className="badge badge-primary">{this.props.bpi[this.state.currency].code}</span> 
-          <strong>{this.props.bpi[this.state.currency].rate}</strong>
-        </li>
+          {list}
         </ul>
         <br/>
         <select onChange={e => this.setState({currency: e.target.value})} className="form-control">
@@ -25,3 +45,4 @@ class Prices extends React.Component {
 }
 
 export default Prices;
+
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import Link from 'next/link';
-import Navbar from '../components/Navbar';
+import Fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
+import Prices from '../components/Prices';
 
-const Index = () => (
-  <div>
-    <Navbar/>
-    <h1>Hello Welcome</h1>
-  </div>
+const Index = (props) => (
+  <Layout>  
+    <div>
+      <h1>Hello Welcome</h1>
+      {/* {props.bpi.time.updated} */}
+      <p>Check Current Bitcoin Rate</p>
+      <Prices bpi={props.bpi} />
+    </div>
+  </Layout>
 );
 
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+  // console.log('index.getInitialProps returns ', data.bpi)
+  // const { bpi } = data
+  return {
+    bpi: data.bpi
+  }
+}
+
 export default Index;
+
+//https://www.youtube.com/watch?v=IkOVe40Sy0U
